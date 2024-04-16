91c01353b87f40764c75195ad17227e893438e15
Lucas Rodrigues <lukinhasr1423@gmail.com>
2024-04-16 15:57:16 -0300

    exceções tratadas

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedicos.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedicos.java
@@ -1,25 +1,25 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedicos(
-        @NotBlank
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
-        @NotBlank
+        @NotBlank(message = "Email é obrigatório")
         @Email
         String email,
-        @NotBlank
+        @NotBlank(message = "Formato do email é inválido")
         String telefone,
-        @NotBlank
+        @NotBlank(message = "CRM é obrigatório")
         @Pattern(regexp = "\\d{4,6}")
         String crm,
-        @NotNull
+        @NotNull(message = "Especialidade é obrigatória")
         Especialidade especialidade,
-        @NotNull
+        @NotNull(message = "Dados do endereço são obrigatórios")
         @Valid
         DadosEndereco endereco) {}
